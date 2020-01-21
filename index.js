ebc6e40984a9c9d69a53978396ce42a2763dc51f
jasper <msavantfou@gmail.com>
2020-01-21 21:27:10 +1100

    almost ready

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const util=require("util");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+
+const pdf = require("pdf-creator-node");
 
 let queryUrl;
 const googleMapsClient = require('@google/maps').createClient({
@@ -18,25 +22,27 @@ const questions = [{
 }
 ]
 let location;
+let color;
 inquirer
   .prompt(questions)
-  .then(function (username) {
-    console.log(username);
-    queryUrl = `https://api.github.com/users/${username.username}`;
+  .then(function (answers) {
+    console.log(answers);
+    queryUrl = `https://api.github.com/users/${answers.username}`;
+    color=answers.color;
     axios
       .get(queryUrl)
       .then(function (res) {
         //console.log(res.data);
-        console.log("user name: " + res.data.login);
-        console.log("website: " + res.data.url);
-        console.log("blog: " + res.data.blog);
-        console.log("user bio: " + res.data.bio);
-        console.log("Public repos: " + res.data.public_repos);
-        console.log("Number of followers: " + res.data.followers);
-        console.log("Number of following: " + res.data.following);
+        const username = res.data.login;
+        const website = res.data.url;
+        const blog = res.data.blog;
+        const userBio = res.data.bio;
+        const publicRepos = res.data.public_repos;
+        const numFollowers = res.data.followers;
+        const numFollowing = res.data.following;
         location = res.data.location;
         console.log("Location: " + res.data.location);
-        console.log(res.data.avatar_url);
+        const imgUrl = res.data.avatar_url;
 
         googleMapsClient.geocode({ address: location })
           .asPromise()
@@ -46,6 +52,58 @@ inquirer
           .catch((err) => {
             console.log(err);
           });
+        const profile = `<html>
+
+        <head>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+        </head>
+        
+        <body>
+            <div class="card" style="width: 18rem;background-color: ${color};">
+                <img class="card-img-top" src="${imgUrl}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${username}</h5>
+                    <p class="card-text">${userBio}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Followers: ${numFollowers}</li>
+                    <li class="list-group-item">Following: ${numFollowing}</li>
+                    <li class="list-group-item">Public repos: ${publicRepos}</li>
+                    <li class="list-group-item">Location: ${location}</li>
+                </ul>
+                <div class="card-body">
+                    <a href="# ${website}" class="card-link">website</a>
+                    <a href="#" class="card-link">${blog}</a>
+                </div>
+            </div>
+        
+        </body>
+        
+        </html>
+
+          `;
+        fs.writeFile("Profile.html", profile, function (err) {
+          if (err) {
+            throw err;
+          }
+        });
+
+        const html = fs.readFileSync('Profile.html', 'utf8');
+        const options={};
+        const document={
+          html:profile,
+          path:"./profile.pdf"
+        };
+        pdf.create(document, options)
+          .then(res => {
+            console.log(res)
+          })
+          .catch(error => {
+            console.error(error)
+          });
+
       });
   });
 
